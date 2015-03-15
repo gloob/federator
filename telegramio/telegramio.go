477fa3102a0da1884640e8addcfe48c1414be74b
Alejandro Leiva <alejandro.leiva@cocomore.com>
2015-03-15 21:11:17 +0100

    Using our only method exposed in telegram package.

--- telegramio/telegramio.go
+++ telegramio/telegramio.go
@@ -2,5 +2,18 @@ package telegramio
 
 import (
   "github.com/gloob/go-telegram/tgl"
-  "log"
 )
+
+type Conn struct {
+  Host string
+}
+
+func createConnection(host string) *Conn {
+  c := new(Conn)
+  c.Host = host
+
+  tgl_state := new(C.struct_tgl_state)
+  tgl_state = tgl.login(tgl_state)
+
+  return c
+}
